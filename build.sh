8edbd966c52908f4cfd9abd036fbe7e321c46cf1
Julian Kniephoff <Julian.Kniephoff@gmail.com>
2014-05-24 13:22:17 +0200

    Disabled ASAN for lack of OS X support

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 clear
 clear
 cd lumina
-make -j8 ASAN=1 CPLUS_INCLUDE_PATH=../libs/glfw/include
+make -j8 CPLUS_INCLUDE_PATH=../libs/glfw/include
 cd ..
 make clean
-make -j8 ASAN=1 test.bin
+make -j8 test.bin
 echo "----------------------------------------"
 echo ""
 ./test.bin
