977176239f563b3f42beea409649192291d0a92c
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2014-07-21 11:14:37 +0200

    More cleanup and comments.

--- example/test.cpp
+++ example/test.cpp
@@ -66,31 +66,22 @@ void Test::run() {
 }
 
 void Test::execute(HotRenderContext& cnt) {
+
+  // create new framebuffer
   FrameBuffer framebuf;
   framebuf.create();
 
+  // create a texture of the size of the window and attach it to the framebuffer
   Tex2D drawTex;
   drawTex.create(m_win.getSize(), TexFormat::RGB8);
-  // drawTex.prime(1, [](HotTex2D& hot) {
-    // hot.param.filterMode = TexFilterMode::Nearest;
-  // });
-  // framebuf.colors[0] = drawTex;
   framebuf.attachColor(0, drawTex);
 
-  VertexSeq mesh;
-  mesh.create(3 + 3 + 2, 3);
-  
-  // mesh.prime<Vec3f, Color32f>([](HotVertexSeq<Vec3f, Color32f>& m) {
-  //   m.vertex[0].set(Vec3f(-1.f, -1.f, 0.f),
-  //                   Color32f(1.f, 0.f, 0.f));
-  //   m.vertex[1].set(Vec3f(1.f, -1.f, 0.f),
-  //                   Color32f(0.f, 1.f, 0.f));
-  //   m.vertex[2].set(Vec3f(0.f, 1.f, 0.f),
-  //                   Color32f(0.f, 0.f, 1.f));
-  //   m.applyVertexLayout();
-  // });
 
-  mesh.prime([](HotVertexSeq<>& hot) {
+  // create a VertexSeq and fill it with data to represent a triangle
+  VertexSeq triangle;
+  triangle.create(3 + 3 + 2, 3);
+
+  triangle.prime([](HotVertexSeq<>& hot) {
     hot.vertex[0] = {-1.f, -1.f, 0.f, 0.0f, 0.0f, 0.0f,  0.5f,  0.5f};
     hot.vertex[1] = { 1.f, -1.f, 0.f, 0.0f, 0.0f, 0.0f, -0.5f,  0.5f};
     hot.vertex[2] = { 0.f,  1.f, 0.f, 0.0f, 0.0f, 0.0f,  0.5f, -0.5f};
@@ -98,11 +89,11 @@ void Test::execute(HotRenderContext& cnt) {
     hot.applyVertexLayout<Vec3f, Vec3f, Vec2f>();
   });
 
-
+  // generate cube
   auto cube = createBox<VChan::Position, VChan::Normal, VChan::TexUV>(
     Vec3f(1, 1, 1) * 0.8);
 
-  // Shader tests
+  // create two shaders and link them together
   Shader<ShaderType::Vertex> vs;
   vs.compile(loadShaderFromFile("shader/test.vsh"));
   Shader<ShaderType::Fragment> fs;
@@ -111,15 +102,8 @@ void Test::execute(HotRenderContext& cnt) {
   Program p;
   p.create(vs, fs);
 
-  Vec2i dim(16, 16);
-  Image<Color32f> i;
-  i.create(dim);
-  for(auto it : dim) {
-    i[it] = Color32f((it.x + it.y) / (dim.x * 2.f),
-                     (it.x == it.y ? 1.f : 0.f),
-                     (it.x == it.y ? 1.f : 0.f));
-  }
 
+  // load a picture from file and create a texture of it
   auto pic = loadJPEGImage("gfx/ava.jpg");
 
   Tex2D tex;
@@ -132,34 +116,30 @@ void Test::execute(HotRenderContext& cnt) {
   });
 
 
-  // glEnable(GL_CULL_FACE);
-
-  
-
+  // loop until the game is stopped
   while(m_win.isValid() && m_gameRunning) {
+    // poll events
     m_win.update();
 
+    // draw the first pass
     framebuf.prime([&](HotFrameBuffer& hotFrame) {
-      // hotFrame.colors[0].clear(Color32A());
       hotFrame.clearColor(0, Color32fA(1.0f, 0, 0));
       tex.prime(0, [&](HotTex2D& hotTex) {
         TexCont cont(hotTex);
-        // glClear(GL_COLOR_BUFFER_BIT);
 
         p.prime([&](HotProgram& hot) {        
-          hot.draw(cont, mesh, PrimitiveType::Triangle);
+          hot.draw(cont, triangle, PrimitiveType::Triangle);
         });
       });
     });
 
+    // draw second pass
     cnt.getDefaultFrameBuffer().prime([&](HotFrameBuffer& hotFrame){
       hotFrame.clearColor(0, Color32fA(0, 1.0, 0));
       drawTex.prime(0, [&](HotTex2D& hotTex) {
         TexCont cont(hotTex);
-        // glClear(GL_COLOR_BUFFER_BIT);
 
         p.prime([&](HotProgram& hot) {
-          // hot.draw(cont, mesh, PrimitiveType::Triangle);
           hot.draw(cont, cube, PrimitiveType::TriangleStrip);
         });
       });
